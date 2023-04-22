2b25a0696fbc04be5adbfa790826257be8b6fdf8
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-04-22 20:57:36 +0700

    Add antd and pakage redux-firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { useState, useEffect } from 'react';
+import './App.css'
 
-import MuatationRef from './components/useRef/MuatationRef';
-
-import { userCollecion } from './firebase/controller';
-import { DocumentData, QuerySnapshot, onSnapshot } from 'firebase/firestore';
-import { NewUsersType } from './type/users.type';
+import User from "./components/pages/users";
 function App() {
-  const [users, setUsers] = useState<NewUsersType[]>([])
 
-  useEffect(() => {
-    onSnapshot(userCollecion, (snapshot: QuerySnapshot<DocumentData>) => {
-      const fetchUser = snapshot.docs.map(doc => {
-        return {
-          id: doc.id,
-          ...doc.data()
-        }
-      })
-      setUsers(fetchUser)
-    })
-  }, [])
-
-  console.log(users)
   return (
     <div className="App">
-      <h1>List User</h1>
-      <ul>
-        {users && users.length > 0 && users.map((user) => {
-          return <li key={user.id}>{user.lName}</li>
-        })}
-      </ul>
+      <User />
 
-      <MuatationRef />
     </div>
   );
 }
--- src/components/pages/users/index.tsx
+++ src/components/pages/users/index.tsx
@@ -1,8 +1,69 @@
 
+import { useState, useEffect } from 'react';
+import { userCollecion } from '../../../firebase/controller';
+import { DocumentData, QuerySnapshot, onSnapshot } from 'firebase/firestore';
+import { NewUsersType } from '../../../type/users.type';
+import { Button, Table } from 'antd';
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons"
+
+
 
 const User = () => {
+  const columns = [
+    { title: "ID", dataIndex: 'id' },
+    { title: "Name", dataIndex: 'lName' },
+    { title: "Email", dataIndex: 'email' },
+    {
+      title: "Action",
+      render: () => {
+        return (
+          <>
+            <EditOutlined />
+            <DeleteOutlined style={{ color: 'red', marginLeft: "10px" }} />
+          </>
+        )
+      }
+    },
+
+  ]
+
+  const [users, setUsers] = useState<NewUsersType[]>([])
+
+  useEffect(() => {
+    onSnapshot(userCollecion, (snapshot: QuerySnapshot<DocumentData>) => {
+      const fetchUser = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      })
+
+      // const firstObj = fetchUser[0] || {};
+      // const cols = [];
+      // for (const key in firstObj) {
+      //   const col = {
+      //     title: key,
+      //     dataIndex: key,
+
+      //   };
+
+      //   cols.push(col)
+      // }
+      // setColumes(cols)
+
+      setUsers(fetchUser)
+    })
+  }, [])
+
+
   return (
-    <div>User</div>
+    <div>
+      <Button>Add User</Button>
+      <h1>List User</h1>
+      <Table columns={columns} dataSource={users}
+        scroll={{ y: 500 }}
+      />
+    </div>
   )
 }
 
