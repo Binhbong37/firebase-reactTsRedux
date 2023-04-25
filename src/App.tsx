a4547c44db87d8957573142be8bb6ef464c41d4c
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-04-25 22:50:43 +0700

    2. Phân chia các trang layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import User from "./components/pages/users";
-import AddUser from './components/pages/users/AddUser';
-
-
+import Header from './components/Header';
+import Home from './pages/Home';
+import EventPage from './pages/EventPage';
+import ContactPage from './pages/ContactPage';
+import E404Page from './components/E404Page';
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<User />} />
-        <Route path='/addUser' element={<AddUser />} />
-
+        <Route path='/' element={<Home />} />
+        <Route path='/event' element={<EventPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='*' element={<E404Page />} />
       </Routes>
 
     </BrowserRouter>
