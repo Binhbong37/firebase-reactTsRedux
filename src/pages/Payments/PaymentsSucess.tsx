c48a477677c1d827f56a9429548bf460cefaa5b2
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-05-01 00:21:30 +0700

    page payment success

--- src/pages/Payments/PaymentsSucess.tsx
+++ src/pages/Payments/PaymentsSucess.tsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Col, Row, Card } from 'antd';
 
 const PaymentsSucess: React.FC = () => {
   return (
-    <div>
-      <h2 className='title-page'>Thanh toán thành công</h2>
-      <div>
-        <button className='btn btn-contact'>Tải về</button>
-        <button className='btn btn-contact'>Gửi email</button>
+    <>
+      <div className='wrapper'>
+        <h2 className='title-page' style={{ textAlign: 'center' }}>Thanh toán thành công</h2>
+        <div className='bottom-btn'>
+          <button className=''>Tải về</button>
+          <button className=''>Gửi email</button>
+        </div>
+        <div className="payment">
+          <div className="payment-center">
+            <div className='grid'>
+              <Row justify="space-evenly">
+                <Col span={4}><Card title="Card title" bordered={true}>
+                  Card content
+                </Card></Col>
+                <Col span={4}><Card title="Card title" bordered={true}>
+                  Card content
+                </Card></Col>
+                <Col span={4}><Card title="Card title" bordered={true}>
+                  Card content
+                </Card></Col>
+                <Col span={4}><Card title="Card title" bordered={true}>
+                  Card content
+                </Card></Col>
+              </Row>
+            </div>
+            <div className='bottom-layout'>
+              <p>Số lượng: 12 vé</p>
+              <p>Trang 1/3</p>
+            </div>
+          </div>
+        </div>
+
       </div>
-    </div>
+
+    </>
   )
 }
 
 export default PaymentsSucess;
-
-
-
-/* 
-position: absolute;
-width: 1920px;
-height: 1104px;
-left: 0px;
-top: 0px;
-
-*/
