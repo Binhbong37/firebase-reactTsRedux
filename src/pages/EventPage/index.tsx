5b829f3d1637bf9788ae678b0251166a14944cf7
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-05-07 14:11:33 +0700

    Sửa lại sự kiện

--- src/pages/EventPage/index.tsx
+++ src/pages/EventPage/index.tsx
@@ -1,68 +1,51 @@
 import React from "react";
 import { Row, Col } from 'antd';
 import { Card } from 'antd';
-import { Link } from "react-router-dom";
-
 const { Meta } = Card;
 
 const EventPage: React.FC = () => {
   return (
-    <div>
-      <h2 className="title-page" style={{ textAlign: "center" }}>Sự kiện nổi bật</h2>
-      <div className="wrapper">
-        <div className="event-layout">
-          <Row>
-            <Col span={6}>
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
-
-                <Link to={'/event/event-1'}>
-
-                  <button
-
-                    className="">Xem chi tiết</button>
-                </Link>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
-                <button className="">Xem chi tiết</button>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
-                <button className="">Xem chi tiết</button>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
-                <button className="">Xem chi tiết</button>
-              </Card>
-            </Col>
-
-          </Row>
-        </div>
+    <div className="container-events">
+      <div className="container-events__main">
+        <Row gutter={16}>
+          <Col className="gutter-row" span={6}>
+            <div> <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card></div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div> <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card></div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div> <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card></div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div> <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card></div>
+          </Col>
+        </Row>
       </div>
-
     </div>
   )
 }
