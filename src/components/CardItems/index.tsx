42b1ea15fe511687ff8fb793b0b64989cbf56c5a
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-05-11 22:35:11 +0700

    paymentsucess

--- src/components/CardItems/index.tsx
+++ src/components/CardItems/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import qr from '../../public/imgs/QR.png'
+import qr from '../../public/imgs/QR.png';
+import Check from '../Icons/Check';
 
 
 
@@ -9,6 +10,12 @@ const CardItem: React.FC = () => {
     <div className='card'>
       <img src={qr} alt='qrcode' />
       <h2>ALT20210501</h2>
+      <p className='port-ticket'>VÉ CỔNG</p>
+      <p>---</p>
+      <p className='dateUse'>Ngày sử dụng: 31/05/2021</p>
+      <div className='checkIcon'>
+        <Check />
+      </div>
     </div>
 
   )
